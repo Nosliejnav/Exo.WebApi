3e2087e7827d872b338015906c6a1ff76e05516b
Nosliejnav <vanjeilsonm10@gmail.com>
2024-08-03 18:46:29 -0300

    Add  Forma de autenticação.

--- Exo.WebApi/Program.cs
+++ Exo.WebApi/Program.cs
@@ -1,16 +1,56 @@
 using Exo.WebApi.Contexts;
 using Exo.WebApi.Repositories;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<ExoContext, ExoContext>();
 builder.Services.AddControllers();
+
+///
+// Forma de autenticação.
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = "JwtBearer";
+    options.DefaultChallengeScheme = "JwtBearer";
+})
+// Parâmetros de validade do token.
+.AddJwtBearer("JwtBearer", options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        // Validade quem está solicitando.
+        ValidateIssuer = true,
+        // Validade quem está recebendo.
+        ValidateAudience = true,
+        // Define se o tempo de expiração será validado.
+        ValidateLifetime = true,
+        //Criptografia e validação da chave de autenticação.
+        IssuerSigningKey = new
+    SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("exoapi-chave-autenticacao")),
+        // Valida o tempo de expiração do token.
+        ClockSkew = TimeSpan.FromMinutes(30),
+        // Nome do issuer, da origem.
+        ValidIssuer = "exoapi.webapi",
+        //Nome do audience, para o destino.
+        ValidAudience = "exoapi.webapi"
+    };
+});
+// Fim da Forma de autenticação.
+///
+
 builder.Services.AddTransient<ProjetoRepository, ProjetoRepository>();
 builder.Services.AddTransient<UsuarioRepository, UsuarioRepository>();
 
 var app = builder.Build();
 
 app.UseRouting();
+
+// Habilita a autenticação.
+app.UseAuthentication();
+
+// Habilita a autorização
+app.UseAuthentication();
 
 app.UseEndpoints(endpoints =>
 {
